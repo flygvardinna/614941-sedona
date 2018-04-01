495509610d07060b91c27be297ab2a61a47bffdd
flygvardinna <36817251+flygvardinna@users.noreply.github.com>
2018-04-01 16:51:00 +0700

    Поправила js, паддинги в отелях

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var button = document.querySelector(".search-form-button");
 var modal = document.querySelector(".modal");
 
+modal.classList.add("modal-hide");
+
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   modal.classList.toggle("modal-show");
